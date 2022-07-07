6733d6ea6bdda97c252cbe1f8b9c810d2c892b88
JonasGN <j.goncalvesneto@outlook.com>
2022-07-07 14:56:47 -0300

    all tests passing

--- src/helpers/formatters.ts
+++ src/helpers/formatters.ts
@@ -2,5 +2,5 @@ import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
 export function toLocaleDate(date: string): string {
-  return format(new Date(date), 'PP', { locale: ptBR });
+  return format(new Date(date), 'dd MMM yyyy', { locale: ptBR });
 }
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,3 +1,8 @@
+/* eslint-disable react/no-danger */
+/* eslint-disable react/jsx-fragments */
+import { Fragment } from 'react';
+import { asHTML } from '@prismicio/helpers';
+import { useRouter } from 'next/router';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
 import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
@@ -9,13 +14,6 @@ import { getPrismicClient } from '../../services/prismic';
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
-type PostContent = {
-  heading: string;
-  body: {
-    text: string;
-  }[];
-};
-
 interface Post {
   first_publication_date: string | null;
   data: {
@@ -24,7 +22,12 @@ interface Post {
       url: string;
     };
     author: string;
-    content: PostContent[];
+    content: {
+      heading: string;
+      body: {
+        text: string;
+      }[];
+    }[];
   };
 }
 
@@ -33,9 +36,32 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps): JSX.Element {
-  const { first_publication_date: moment, data } = post;
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return (
+      <div className={`${commonStyles.widthContainer} ${styles.loading}`}>
+        <span className={styles.loading}>Carregando...</span>
+      </div>
+    );
+  }
 
   const articleClasses = `${commonStyles.widthContainer} ${styles.contentContainer}`;
+  const { first_publication_date: moment, data } = post;
+
+  const totalWords = data.content.reduce((total, item) => {
+    let sum = total;
+    const headingSum = item.heading.split(' ').filter(head => head).length;
+    item.body.forEach(content => {
+      sum += content.text.split(' ').filter(con => con).length;
+    });
+
+    sum += headingSum;
+    return sum;
+  }, 0);
+
+  const readTimeEstimateInMinutes = Math.ceil(totalWords / 200);
+
   return (
     <>
       <Head>
@@ -53,10 +79,30 @@ export default function Post({ post }: PostProps): JSX.Element {
           <h1>{data.title}</h1>
 
           <PostInfoContainer>
-            <PostInfo info={moment} element="time" icon={<FiCalendar />} />
+            <PostInfo
+              info={toLocaleDate(moment)}
+              element="time"
+              icon={<FiCalendar />}
+            />
             <PostInfo info={data.author} icon={<FiUser />} />
-            <PostInfo info="4min" icon={<FiClock />} />
+            <PostInfo
+              info={`${readTimeEstimateInMinutes} min`}
+              icon={<FiClock />}
+            />
           </PostInfoContainer>
+
+          <div className={styles.content}>
+            {data.content.map(item => (
+              <Fragment key={item.heading}>
+                {item.heading ? <h2>{item.heading}</h2> : null}
+                <section
+                  dangerouslySetInnerHTML={{
+                    __html: asHTML(item.body),
+                  }}
+                />
+              </Fragment>
+            ))}
+          </div>
         </article>
       </main>
     </>
@@ -65,34 +111,38 @@ export default function Post({ post }: PostProps): JSX.Element {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const prismic = getPrismicClient({});
-  // const posts = await prismic.getByType(TODO);
+  const posts = await prismic.getByType('post', { pageSize: 2 });
 
-  return { paths: [], fallback: 'blocking' };
+  const paths = posts.results.map(post => ({
+    params: { slug: post.uid },
+  }));
+
+  return { paths, fallback: true };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const prismic = getPrismicClient({});
   const response = await prismic.getByUID('post', String(params.slug));
 
-  const content = response.data.content as PostContent[];
-  const postContent = content.map(post => ({
-    heading: post.heading,
-    body: post.body.map(item => ({
-      text: item.text,
-    })),
-  }));
+  const { first_publication_date, data, uid } = response;
 
-  const post: Post = {
-    first_publication_date: toLocaleDate(response.first_publication_date),
+  const post = {
+    uid,
+    first_publication_date,
     data: {
-      title: response.data.title,
-      author: response.data.author,
-      banner: { url: response.data.banner.url ?? '' },
-      content: postContent,
+      title: data.title,
+      subtitle: data.subtitle,
+      banner: { url: data.banner.url },
+      author: data.author,
+      content: data.content.map(con => ({
+        heading: con.heading ?? '',
+        body: [...con.body],
+      })),
     },
   };
 
   return {
     props: { post },
+    revalidate: 60 * 60, // 1 minute
   };
 };
